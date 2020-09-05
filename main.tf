3f99b34c04ed724ab524005d20e89d30b3eb4a8b
Vlad <vlad@ghn.me>
2020-09-05 14:06:07 -0500

    Fix resource

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "aws_cloudtrail" "cloudtrail" {
   s3_bucket_name                = aws_s3_bucket.cloudtrail.id
   include_global_service_events = true
   is_multi_region_trail         = true
-  cloud_watch_logs_group_arn    = "${aws_cloudwatch_log_group.example.arn}:*" # CloudTrail requires the Log Stream wildcard
+  cloud_watch_logs_group_arn    = "${aws_cloudwatch_log_group.cloudtrail.arn}:*" # CloudTrail requires the Log Stream wildcard
   cloud_watch_logs_role_arn     = aws_iam_role.cloudtrail.arn
 
   tags = var.common_tags
